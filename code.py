a9839c4fc27d047bf0d21936ac18bcb36ded066a
Olivia <22328406@bucks.ac.uk>
2025-12-03 22:07:00 +0000

    project setup - creating the book and library class

---
+++ code.py
@@ -0,0 +1,11 @@
+
+#creating the book class which features the title, author and ISBN
+class Book:
+    def__init__(self, title, author, ISBN)
+    self.title = title
+    self.author = author
+
+#creating the library class which holds the books
+class Library:
+    def__init__(self)
+    self.books = []
