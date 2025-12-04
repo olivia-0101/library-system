5c6e869c8cd1fc1d6b2dc02f3760f423b19ed963
Olivia <22328406@bucks.ac.uk>
2025-12-04 20:46:09 +0000

    hard-coded a small book dataset

--- code.py
+++ code.py
@@ -9,3 +9,17 @@ class Book:
 class Library:
     def__init__(self)
     self.books = []
+
+#simple book dataset
+books = [
+    Book("Classical Mythology", "Mark P. O. Morford", "0195153448"),
+    Book("Clara Callan", "Richard Bruce Wright", "0002005018"),
+    Book("Decision in Normandy", "Carlo D'Este", "0060973129"),
+    Book("Flu: The Story of the Great Influenza Pandemic of 1918 and the Search for the Virus That Caused It", "Gina Bari Kolata", "0374157065"),
+    Book("The Mummies of Urumchi", "E. J. W. Barber", "0393045218"),
+    Book("The Kitchen God's Wife", "Amy Tan", "0399135782"),
+    Book("What If?: The World's Foremost Military Historians Imagine What Might Have Been", "Robert Cowley", "0425176428"),
+    Book("PLEADING GUILTY", "Scott Turow", "0671870432"),
+    Book("Under the Black Flag: The Romance and the Reality of Life Among the Pirates", "David Cordingly", "0679425608"),
+    Book("Where You'll Find Me: And Other Stories", "Ann Beattie", "074322678X")
+]
