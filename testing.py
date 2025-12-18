cb40b15f1b4fc81072b2669c6bd9c6dde1be6745
Olivia <22328406@bucks.ac.uk>
2025-12-18 16:15:48 +0000

    Testing Returning a Book

--- testing.py
+++ testing.py
@@ -3,8 +3,10 @@ from library_system import User, Library
 Mary = User(234)
 library = Library()
 
-library.borrow_book(Mary, "classical mytholog")
+library.borrow_book(Mary, "classical mythology")
+library.return_book(Mary, "classical mythology")
 
-print(Mary.id)
 for book in Mary.borrowed_books:
     print(book.title)
+
+print(library.books)
